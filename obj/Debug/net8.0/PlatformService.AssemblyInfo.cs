55494e434a178f533f11f11bb950551d7ad78a6c
dcrawford027 <dustin.crawford.2@gmail.com>
2024-03-05 15:14:27 -0500

    Created get by id and create platform endpoints

--- obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05b99652c4b9b5f7e293b5c0f9c318cda725832b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7534ea71270fc19ab4869e2a5b8dedc7e04f67e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
