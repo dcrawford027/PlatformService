1a075504c4058848af2c49214da118024fd4540c
dcrawford027 <dustin.crawford.2@gmail.com>
2024-03-05 10:22:59 -0500

    Created Platform Repository

--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
 using Microsoft.EntityFrameworkCore;
-using PlatformService.Models.Data;
+using PlatformService.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("InMem"));
+
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
